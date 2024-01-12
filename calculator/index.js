7b27f1344b34779ba2c35fd6442d0865bbc630fb
Georgie <georgie.farrar@hotmail.com>
2024-01-12 22:40:13 +1100

    Create evaluate, operate and set operation functions

--- calculator/index.js
+++ calculator/index.js
@@ -1,16 +1,91 @@
-const add = function(a, b) {
+let firstOperand = '';
+let secondOperand = '';
+let currentOperator = null; 
+let reset = false;
+
+const operandBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const decimalBtn = document.querySelector("#decimal");
+const equalsBtn = document.querySelector("#equals");
+const backspaceBtn = document.querySelector("#backspace-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+
+clearBtn.addEventListener('click', clear);
+backspaceBtn.addEventListener('click', backspace);
+decimalBtn.addEventListener('click', decimal);
+equalsBtn.addEventListener('click', evaluate)
+
+operandBtns.forEach((button) =>
+    button.addEventListener('click', () => operand(button.textContent))
+)
+operatorBtns.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function operand(number) {
+    if (output.textContent === '0' || reset) 
+        resetScreen()
+    output.textContent += number;
+}
+
+function resetScreen() {
+    output.textContent = '';
+    reset = false;
+}
+
+function clear() {
+    output.textContent = '0';
+    input.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperator = null;
+  }
+
+function backspace() {
+    output.textContent = output.textContent.toString().slice(0, -1);
+}
+
+function decimal() {
+    if (reset) resetScreen();
+    if (output.textContent === '') 
+        output.textContent = '0';
+    if (output.textContent.includes('.')) return
+        output.textContent += '.'; 
+}
+
+function setOperation(operator) {
+    if (currentOperator !== null) evaluate()
+    firstOperand = output.textContent
+    currentOperator = operator
+    input.textContent = `${firstOperand} ${currentOperator}`
+    reset = true
+}
+
+function evaluate() {
+    if (currentOperator === null || reset) return
+    else {
+        secondOperand = output.textContent;
+        output.textContent = roundResult(operate(currentOperator, firstOperand, secondOperand));
+        input.textContent = `${firstOperand} ${currentOperator} ${secondOperand} =`;
+        currentOperator = null;
+    }
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+}
+
+function add(a, b) {
 	return a + b;
 };
 
-const subtract = function(a, b) {
+function subtract(a, b) {
 	return a - b;
 };
 
-const multiply = function(a, b) {
-    return a * b;
-};
-
-const divide = function(a, b) {
+function divide(a, b) {
     if (b === 0) {
         alert("DUH! You can't divide by 0!");
     }
@@ -19,13 +94,42 @@ const divide = function(a, b) {
     }
 };
 
-const power = function(a, b) {
+function multiply(a, b) {
+    return a * b;
+};
+
+function percentage(a, b) {
+    return (a / 100) * b;
+};
+
+function power(a, b) {
 	return Math.pow(a, b);
 };
 
-const percentage = function(a, b) {
-    return (a / 100) * b;
-};
+function operate(operator, a, b) {
+
+    a = Number(a);
+    b = Number(b);
+
+    switch (operator) {
+        case '+':
+            return add(a, b)
+        case '−':
+            return subtract(a, b)
+        case '÷':
+            return divide(a, b)
+        case '×':
+            return multiply(a, b)
+        case '٪':
+            return percentage(a, b)
+        case 'xⁿ':
+            return power(a, b)
+        default:
+            return null;
+    }
+}
+
+
 
 /* const sum = function(array) {
 	return array.reduce((total, current) => total + current, 0);
